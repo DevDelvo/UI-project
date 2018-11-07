ec530d5c0fac399d221d0d7d812d08408c5ce2d3
DevDelvo <kevinbdelvo@gmail.com>
2018-11-07 01:16:06 -0500

    set up sidebar menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Button, Menu, Icon, Segment, Sidebar } from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 
-export default () => {
+export default (props) => {
+  const styles = {
+    largeMenu: {
+      marginTop: '0px',
+      width: '100%',
+      height: '20px',
+    },
+    drawerButton: {
+      margin: "0px",
+    }
+  }
+  const { visible, handleShowClick } = props;
   return (
-    <Menu style={{ marginTop:'0px',}} size={'large'}>
+    <Menu style={styles.largeMenu} size={'large'}>
+      {/* <Menu.Menu style={styles.drawerButton}>
+        <Menu.Item><Icon name="bars" /></Menu.Item>
+      </Menu.Menu> */}
+      <Button.Group>
+        <Button disabled={visible} onClick={handleShowClick}>
+          <Icon name="bars" />
+        </Button>
+      </Button.Group>
+
       <Menu.Item><Link to='/'>Azure UI</Link></Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item><Link to='/someLink'><Icon name='tasks' />Some Link</Link></Menu.Item>
@@ -12,3 +32,77 @@ export default () => {
     </Menu>
   );
 };
+
+// const styles = {
+//     largeMenu: {
+//       marginTop: '0px',
+//       width: '100%',
+//       height: '20px',
+//     },
+//     drawerButton: {
+//       margin: "0px",
+//     }
+//   }
+// class Header extends React.Component {
+//   state = { visible: false }
+
+//   handleHideClick = () => this.setState({ visible: false })
+//   handleShowClick = () => this.setState({ visible: true })
+//   handleSidebarHide = () => this.setState({ visible: false })
+
+//   render() {
+//     const { visible } = this.state;
+
+//     return (
+//       <div>
+//         <Button.Group>
+//           <Button disabled={visible} onClick={this.handleShowClick}>
+//             <Icon name="bars" />
+//           </Button>
+//         </Button.Group>
+
+//         <Sidebar.Pushable as={Segment}>
+//           <Sidebar
+//             as={Menu}
+//             animation='overlay'
+//             icon='labeled'
+//             inverted
+//             onHide={this.handleSidebarHide}
+//             vertical
+//             visible={visible}
+//             width='thin'
+//           >
+//             <Menu.Item as='a'>
+//               <Icon name='home' />
+//               Home
+//             </Menu.Item>
+//             <Menu.Item as='a'>
+//               <Icon name='gamepad' />
+//               Games
+//             </Menu.Item>
+//             <Menu.Item as='a'>
+//               <Icon name='camera' />
+//               Channels
+//             </Menu.Item>
+//           </Sidebar>
+
+//         <Sidebar.Pusher>
+//             <Segment basic>
+//               <Menu style={styles.largeMenu} size={'large'}>
+//                 {/* <Menu.Menu style={styles.drawerButton}>
+//                   <Menu.Item><Icon name="bars" /></Menu.Item>
+//                 </Menu.Menu> */}
+//                 <Menu.Item><Link to='/'>Azure UI</Link></Menu.Item>
+//                 <Menu.Menu position="right">
+//                   <Menu.Item><Link to='/someLink'><Icon name='tasks' />Some Link</Link></Menu.Item>
+//                 </Menu.Menu>
+//               </Menu>
+//             </Segment>
+//           </Sidebar.Pusher>
+//         </Sidebar.Pushable>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import {Container} from 'semantic-ui-react';
+import {Container, Menu, Icon, Sidebar} from 'semantic-ui-react';
 import Header from './Header';
 
-export default props => {
-  return (
-    <Container>
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
-      <Header />
-      {props.children}
-    </Container>
-  );
-};
+// export default props => {
+//   return (
+//     <Container>
+//       <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
+//       <Header />
+//       {props.children}
+//     </Container>
+//   );
+// };
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarVisible: false,
+    }
+  }
+
+  handleShowClick = () => {
+    this.setState({ sidebarVisible: true })
+  }
+
+  handleSidebarHide = () => {
+    this.setState({ sidebarVisible: false })
+  }
+
+  render() {
+    const { sidebarVisible } = this.state;
+    const props = this.props;
+    return (
+      <Container>
+
+        <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={sidebarVisible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
+        <Header visible={sidebarVisible} handleShowClick={this.handleShowClick}/>
+        {props.children}
+
+      </Container>
+    );
+  };
+}
+
+
+export default Layout;
